9fbcaf674cd9b58fd262b78802b37ef39b1f9859
CRISTIANO CORREA DE MORAES <94192765+cristianocorreamoraes@users.noreply.github.com>
2022-09-08 16:25:26 -0300

    Implementação de funcionalidade de inserção, seleção, alteração e exclusão de livros na controller.
    Implementação de funcionalidade de inserção, seleção, alteração e exclusão de livros na controller.

--- controller/LivroController.js
+++ controller/LivroController.js
@@ -8,23 +8,127 @@ const router = express.Router();
 
 //ROTA DE LISTAGEM DE LIVROS
 router.get('/listarLivros', (req, res)=>{
-    res.send('ROTA DE LISTAGEM DE LIVROS');
+
+    //res.send('ROTA DE LISTAGEM DE LIVROS');
+    livroModel.findAll()
+    .then(
+        (livros)=>{
+            res.status(200).send(livros);
+        }
+    )
+    .catch(
+        (error)=>{
+            res.status(500).json({"MSG":error});
+        }
+    );
+});
+
+//ROTA DE LISTAGEM DE LIVROS POR CÓDIDO DO LIVRO
+router.get('/listarLivros/:cod_livro', (req, res)=>{
+
+    let { cod_livro } = req.params;
+
+    livroModel.findAll({
+        where:{
+            cod_livro
+        }
+    })
+    .then(
+        (livro)=>{
+            res.status(200).send(livro);
+        }
+    )
+    .catch(
+        (error)=>{
+            res.status(500).json({"MSG":error});
+        }
+    );
+
+    /** POR CHAVE PRIMÁRIA **/
+    // livroModel.findByPk(cod_livro)
+    // .then(
+    //     (livro)=>{
+    //         res.status(200).send(livro);
+    //     }
+    // )
+    // .catch(
+    //     (error)=>{
+    //         res.status(500).json({"MSG":error});
+    //     }
+    // );
+
 });
 
 //ROTA DE CADASTRO DE LIVROS
 router.post('/cadastrarLivros', (req, res)=>{
-    console.log(req.body);
-    res.send('ROTA DE CADASTRO DE LIVROS');
+    //console.log(req.body);
+    //res.send('ROTA DE CADASTRO DE LIVROS');
+
+    let { titulo, descricao, imagem } = req.body;
+    //console.log(req.body);
+    livroModel.create({
+        titulo,
+        descricao,
+        imagem
+    }).then(
+        ()=>{
+            res.status(201).json({"MSG":"LIVRO INSERIDO COM SUCESSO!"});
+        }
+    ).catch(
+        (error)=>{
+            res.status(500).json({"MSG":error});
+        }
+    );
+
 });
 
 //ROTA DE ALTERAÇÃO DE LIVROS
 router.put('/alterarLivros', (req, res)=>{
-    res.send('ROTA DE EDIÇÃO DE LIVROS');
+
+    //res.send('ROTA DE EDIÇÃO DE LIVROS');
+
+    let { titulo, descricao, imagem, cod_livro } = req.body;
+
+    livroModel.update(
+        {
+        titulo,
+        descricao,
+        imagem
+        },
+        {where:{cod_livro}}
+    )
+    .then(
+        ()=>{
+            res.status(200).json({"MSG":"LIVRO ALTERADO COM SUCESSO."});
+        }
+    )
+    .catch(
+        (error)=>{
+            res.status(500).json({"MSG":error});
+        }
+    );
 });
 
 //ROTA DE EXCLUSÃO DE LIVROS
-router.delete('/excluirLivros', (req, res)=>{
-    res.send('ROTA DE EXCLUIR DE LIVROS');
+router.delete('/excluirLivros/:cod_livro', (req, res)=>{
+    //res.send('ROTA DE EXCLUIR DE LIVROS');
+    let {cod_livro} = req.params;
+
+    livroModel.destroy({
+        where:{cod_livro}
+    })
+    .then(
+        ()=>{
+            res.status(200)
+            .json({"MSG":"LIVRO EXCLUÍDO COM SUCESSO."});
+        }
+    )
+    .catch(
+        (error)=>{
+            res.status(500)
+            .json({"MSG":error});
+        }
+    );
 });
 
 module.exports = router;
